87d027f0508370223ec596ebd17a3252c4a10705
jgranadosiastic <jgranados.iastic@gmail.com>
2021-11-23 10:30:48 -0600

    Adding DB

---
+++ Dump20211123.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `calculos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `calculos`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: calculos
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `historial`
+--
+
+DROP TABLE IF EXISTS `historial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial` (
+  `id_historial` int NOT NULL AUTO_INCREMENT,
+  `numero1` int NOT NULL,
+  `numero2` int NOT NULL,
+  `operacion` varchar(45) NOT NULL,
+  `resultado` varchar(150) NOT NULL,
+  PRIMARY KEY (`id_historial`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial`
+--
+
+LOCK TABLES `historial` WRITE;
+/*!40000 ALTER TABLE `historial` DISABLE KEYS */;
+INSERT INTO `historial` VALUES (1,1,2,'ADD','3'),(2,1,2,'ADD','3'),(3,1,3,'BINARY','1, 11'),(4,1,555,'MULTI','1, 101001101'),(5,33,22,'ADD','55'),(6,22,3,'MULTI','66'),(7,22,3,'BINARY','10110, 11'),(8,23,2,'GREATER','23'),(9,23,22,'ADD','45'),(10,23,4,'GREATER','23'),(11,22,109,'BINARY','10110, 1101101'),(12,2,332,'ADD','334'),(13,23,22,'ADD','45'),(14,232,2,'ADD','234'),(15,2,3,'ADD','5'),(16,1,2,'ADD','3'),(17,56,6,'GREATER','56'),(18,55,44,'BINARY','110111, 101100'),(19,23,333,'GREATER','333'),(20,23,33,'MULTI','759'),(21,23,33,'BINARY','10111, 100001'),(22,12,1,'ADD','13'),(23,23,33,'ADD','56'),(24,100,200,'ADD','300'),(25,44,55,'GREATER','55'),(26,23,444,'MULTI','467'),(27,23,100,'MULTI','2300'),(28,1,2,'ADD','3'),(29,1,2,'ADD','3'),(30,1,2,'ADD','3'),(31,1,2,'ADD','3'),(32,1,2,'ADD','3'),(33,1,2,'ADD','3'),(34,1,2,'ADD','3');
+/*!40000 ALTER TABLE `historial` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-23 10:29:57
